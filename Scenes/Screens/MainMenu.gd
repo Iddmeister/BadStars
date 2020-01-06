e764f3568971a15fe266a4dc9f09633049851aea
Iddmeister <iddonagenda.t@gmail.com>
2020-01-06 19:52:43 +0000

    Loading Screen

--- Scenes/Screens/MainMenu.gd
+++ Scenes/Screens/MainMenu.gd
@@ -4,7 +4,7 @@ var currentCharacter = 0
 
 var characters = {
 	
-	Globals.characters.SHMELLY:{"icon":"res://Graphics/Characters/Shmelly.png", "info":"Schmelly: Faster Than Light but generally bad"},
+	Globals.characters.SHMELLY:{"icon":"res://Graphics/Characters/Shmelly.png", "info":"Shmelly: Faster Than Light but generally bad"},
 	Globals.characters.CLOT:{"icon":"res://Graphics/Characters/Clot.png", "info":"Clot: Has red hair"},
 	Globals.characters.SALMON:{"icon":"res://Graphics/Characters/Salmon.png", "info":"Salmon: ..."},
 	
