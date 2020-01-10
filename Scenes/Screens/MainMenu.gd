f81f7eed164e782d091428badbe3309519868b68
sWordy07 <59703206+sWordy07@users.noreply.github.com>
2020-01-10 07:40:54 +0000

    Add files via upload

--- Scenes/Screens/MainMenu.gd
+++ Scenes/Screens/MainMenu.gd
@@ -10,6 +10,7 @@ var characters = {
 	Globals.characters.ELSCRIMO:{"icon":"res://Graphics/Characters/ElScrimo.png", "info":"El Scrimo: REEEEEEEEEEEEEEE"},
 	Globals.characters.WILL:{"icon":"res://Graphics/Characters/WillSmith.png", "info":"Will Smith: Blue"},
 	Globals.characters.BARREL:{"icon":"res://Graphics/Characters/Barryl.png", "info":"Barrel: is barrel"},
+	Globals.characters.POKO:{"icon":"res://Graphics/Characters/Poko.png", "info":"Poko: Buenos Dias"}
 	}
 
 func _ready():
