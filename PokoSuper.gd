bfd870784e6989c7a464fa5cf0f3c83e2aea1526
sWordy07 <59703206+sWordy07@users.noreply.github.com>
2020-01-13 18:07:54 +0000

    Add files via upload

---
+++ PokoSuper.gd
@@ -0,0 +1,42 @@
+extends Super
+
+export var leechAmount = 70
+export var maxLeech = 3
+
+var bodiesLeeched = 0
+
+func _ready():
+	$Aim.visible = false
+	pass
+	
+func aimVisible(val:bool):
+	$Aim.visible = val
+	pass
+	
+remotesync func super(id:int):
+	
+	$Animation.play("Whoosh")
+	
+	if get_parent().is_network_master():
+		
+		for body in $Range.get_overlapping_bodies():
+			
+			if bodiesLeeched < maxLeech:
+				if body.is_in_group("Player"):
+					if not body.is_in_group("Ally"+String(get_parent().get_network_master())):
+						
+						body.rpc("hit", leechAmount, get_parent().get_network_master())
+						get_parent().health += leechAmount
+						if get_parent().health > get_parent().maxHealth:
+							get_parent().health = get_parent().maxHealth
+						get_parent().ui.setHealth(get_parent().health)
+						bodiesLeeched += 1
+						
+					
+					pass
+		bodiesLeeched = 0
+						
+		
+		pass
+	
+	pass
