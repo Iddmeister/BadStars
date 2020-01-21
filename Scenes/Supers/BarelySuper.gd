ed187afe8cd01c20268f91870cd0d9bb850b4334
sWordy07 <59703206+sWordy07@users.noreply.github.com>
2020-01-21 06:38:20 +0000

    Barely sprite fix

--- Scenes/Supers/BarelySuper.gd
+++ Scenes/Supers/BarelySuper.gd
@@ -7,18 +7,25 @@ extends "res://Scenes/Supers/ShmellySuper.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ChargedBarely.visible = false
+	$Sprite.visible = false
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-remotesync func super(id:int):
-	pId = id
-	enablePoison(true)
-	$ChargedBarely.visible = true
-	if get_tree().is_network_server():
-		$Time.start()
+remotesync func enablePoison(val:bool):
+	poisoning = val
+	if val:
+		$Sprite.visible = true
+		$ChargedBarely.visible = true
+		if get_tree().is_network_server():
+			$HitDelay.start()
+	else:
+		$Sprite.visible = false
+		$ChargedBarely.visible = false
+		if get_tree().is_network_server():
+			$HitDelay.stop()
+	
 	pass
 
 func _on_Time_timeout():
 	rpc("enablePoison", false)
-	$ChargedBarely.visible = false
 	pass
