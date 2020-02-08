757a1bb7145a363f86452a06aae859b345669ea1
Iddmeister <iddonagenda.t@gmail.com>
2020-02-08 15:54:42 +0000

    64Bit area of effect

--- Scenes/Supers/BitSuper.gd
+++ Scenes/Supers/BitSuper.gd
@@ -1,18 +1,50 @@
 extends Super
 
 func _ready():
+	$Aim.polygon = $Area2D/CollisionShape2D.shape.points
+	$Aim.visible = false
 	pass
 	
 remotesync func super(id:int):
 	
 	
 	if not get_parent().is_network_master():
-		$Layer/Panel.visible = true
-	
+		
+		for body in $Area2D.get_overlapping_bodies():
+			
+			if not body.is_in_group("Ally"+String(get_parent().get_network_master())):
+				
+				rpc_id(body.get_network_master(), "show")
+				
+				pass
+			
+			pass
+			
 	if get_tree().is_network_server():
+		
 		$Time.start()
+		
 		pass
 	
+	
+	pass
+	
+remotesync func show():
+	
+	$Layer/Panel.visible = true
+	print("yo")
+	
+	pass
+	
+remotesync func aim(dir:float):
+	
+	rotation = dir
+	
+	pass
+	
+func aimVisible(val:bool):
+	
+	$Aim.visible = val
 	
 	pass
 	
