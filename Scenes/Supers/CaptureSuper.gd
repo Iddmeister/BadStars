4373b494e458e95c3f4263e23b2fad0a0d66a8ea
sWordy07 <59703206+sWordy07@users.noreply.github.com>
2020-03-05 16:39:54 +0000

    hmmm
    i dont know

--- Scenes/Supers/CaptureSuper.gd
+++ Scenes/Supers/CaptureSuper.gd
@@ -23,30 +23,13 @@ remotesync func aim(dir:float):
 	
 func drawInfo():
 	for player in info.keys():
-		draw_line(Vector2(0, 0), target.position-global_position, lineColour, lineWidth)
+		draw_line(Vector2(0, 0), bear.global_position, lineColour, lineWidth)
 		
 	pass
 
-func findTarget():
-	
-	var closest:Player
-	
-	for player in get_tree().get_nodes_in_group("Player"):
-		
-		if not player.get_network_master() == get_parent().get_network_master() and not player.is_in_group("Ally"):
-		
-			if not closest:
-				closest = player
-				continue
-			else:
-				if global_position.distance_to(player.global_position) < global_position.distance_to(closest.global_position):
-					closest = player
-				
-	target = closest
 
 remotesync func super(id:int):
 	info = {}
-	findTarget()
 	on = true
 	bear.rpc("place", global_position)
 	pass
