e026f44fe027f10942735761d4a73024243d4097
Iddmeister <iddonagenda.t@gmail.com>
2020-03-17 20:57:20 +0000

    1.0

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -9,7 +9,7 @@ var killLines = ["destroyed", "rekt", "eliminated", "took out", "brutally murder
 
 var mobile = false
 
-var version = "0.6.3"
+var version = "1.0"
 
 var bounds = Vector2(2144, 1984)
 
